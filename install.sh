edbffd2bca0cee25e4a8ea1114ab1245f2e796e4
Saransh Bhatnagar <saransh8@hotmail.com>
2024-04-18 16:03:21 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -36,4 +36,4 @@ echo_red "Updating package lists again..."
 sudo apt-get update
 
 echo_red "Installing Docker and stuff..."
-sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
